910c9a1a1fef8649bdd659acbc357c2925fd8d24
Vinay Gandhi <vinaymgandhi@gmail.com>
2026-06-10 18:51:20 +0530

    Add EmbeddingProvider abstract class

---
+++ app/services/embeddings/base.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+
+class EmbeddingProvider(ABC):
+    @abstractmethod
+    def embed_documents(self, texts: list[str]) -> list[list[float]]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def embed_query(self, text: str) -> list[float]:
+        raise NotImplementedError
